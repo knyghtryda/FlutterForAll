918595a7feeae3352e4db5bdc40032ecb47316e0
yiguo <yiguo@dfnetresearch.com>
2020-06-28 13:56:43 -0400

    remove debug banner, update home page title

--- slhack/lib/main.dart
+++ slhack/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter For All Home Page'),
     );
   }
 }

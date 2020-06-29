d0ba85acf5d8f69f2f1b554c39c49452597da120
Vinicius Sossella <viniciussossella@gmail.com>
2020-06-28 21:15:27 -0300

    Added time machine initial version

--- slhack/lib/mac_os/desktop_screen.dart
+++ slhack/lib/mac_os/desktop_screen.dart
@@ -1,8 +1,13 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 import 'package:slhack/mac_os/mac_dock.dart';
+import 'package:slhack/mac_os/macos_state.dart';
 import 'package:slhack/mac_os/status_bar.dart';
+import 'package:slhack/mac_os/time_machine.dart';
 
 class MacosDesktopScreen extends StatefulWidget {
   @override
@@ -18,54 +23,95 @@ class _MacosDesktopScreenState extends State<MacosDesktopScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final macosState = Provider.of<MacosState>(context);
+
     return Material(
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          FittedBox(
-            child: Image(image: AssetImage('images/macos-desk-background.jpg')),
-            fit: BoxFit.fill,
+        child: Stack(
+      fit: StackFit.expand,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/macos-desk-background.jpg'),
+              fit: BoxFit.fill,
+            ),
           ),
-          Align(
-            alignment: Alignment.topCenter,
-            child: StatusBar(),
-          ),
-          Align(
-            alignment: Alignment.topRight,
-            child: Padding(
-              padding: const EdgeInsets.only(right: 15, top: 80),
-              child: Column(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(
+                sigmaX: macosState.blurX, sigmaY: macosState.blurY),
+            child: Container(
+              color: Colors.black.withOpacity(0.5),
+              child: Stack(
                 children: [
-                  Image(
-                    image: AssetImage('images/machd.png'),
-                    width: 46,
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: StatusBar(),
                   ),
-                  SizedBox(height: 5),
-                  Text(
-                    'Macintosh\nHDD',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 11,
-                      shadows: [
-                        Shadow(
-                          blurRadius: 5,
-                          color: Colors.black,
-                          offset: Offset(5, 5),
-                        ),
-                      ],
+                  if (!macosState.showTimeMachineWindow)
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 15, top: 80),
+                        child: deskMenu(
+                            AssetImage('images/machd.png'), 'Macintosh\nHDD'),
+                      ),
                     ),
-                  )
+                  if (!macosState.showTimeMachineWindow)
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 15, top: 180),
+                        child: InkWell(
+                          child: deskMenu(
+                              AssetImage('images/time-machine-2019-09-25.png'),
+                              'Time\nMachine'),
+                          onTap: macosState.openTimeMachine,
+                        ),
+                      ),
+                    ),
+                  if (!macosState.showTimeMachineWindow)
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: MacDock(),
+                    ),
                 ],
               ),
             ),
           ),
+        ),
+        if (macosState.showTimeMachineWindow)
           Align(
-            alignment: Alignment.bottomCenter,
-            child: MacDock(),
+            alignment: Alignment.center,
+            child: Stack(
+              children: [TimeMachineSwiper()],
+            ),
+          )
+      ],
+    ));
+  }
+
+  Widget deskMenu(AssetImage assetImg, String text) => Column(
+        children: [
+          Image(
+            image: assetImg, // AssetImage('images/machd.png'),
+            width: 46,
+          ),
+          SizedBox(height: 5),
+          Text(
+            text, //'Macintosh\nHDD',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 11,
+              shadows: [
+                Shadow(
+                  blurRadius: 5,
+                  color: Colors.black,
+                  offset: Offset(5, 5),
+                ),
+              ],
+            ),
           )
         ],
-      ),
-    );
-  }
+      );
 }
--- slhack/lib/main.dart
+++ slhack/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:slhack/command_prompt/command_prompt.dart';
 import 'package:slhack/mac_os/desktop_screen.dart';
+import 'package:slhack/mac_os/macos_state.dart';
 import 'package:slhack/state/game_state.dart';
 
 void main() {
@@ -177,9 +178,14 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Text('macos desk'),
               onPressed: () {
                 Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => MacosDesktopScreen()));
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ChangeNotifierProvider(
+                      create: (_) => MacosState(),
+                      child: MacosDesktopScreen(),
+                    ),
+                  ),
+                );
               }),
           ...corruption,
         ],
